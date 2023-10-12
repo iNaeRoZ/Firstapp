9ad66b1c789fb8d4fadc93e5d5d93f019e1be851
iNaeRoZ <naeroz@outlook.fr>
2023-10-12 11:07:58 +0200

    react 06

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,32 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  //const [count, setCount] = useState(0);
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClick = (event) => {
+    console.log(event.target);
+  };
 
+  const handleClickSuivant = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  const handleCLickPrécedent = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]} />
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+        {pokemonIndex > 0 && (
+          <button onClick={handleCLickPrécedent}>Précedent</button>
+        )}
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button onClick={handleClickSuivant}>Suivant</button>
+        )}
       </div>
     </>
   );
@@ -24,6 +44,23 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
+    imgSrc:
+      "https://shreepng.com/img/Inside/Celebrities/TheRock/Angry%20The%20Rock.png",
   },
 ];
